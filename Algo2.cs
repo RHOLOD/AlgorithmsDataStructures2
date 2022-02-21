f75ab72bd707c1e9e03727536a08cd28a64711e3
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2022-02-21 20:14:45 +0500

    Update Algo2.cs

--- Algo2.cs
+++ Algo2.cs
@@ -48,7 +48,10 @@ namespace AlgorithmsDataStructures2
         {
             BSTFind<T> bSTFind = Seach(Root, key);
             // ищем в дереве узел и сопутствующую информацию по ключу
-
+            if (bSTFind.NodeHasKey == false)
+            {
+                bSTFind.Node = null;
+            }
             return bSTFind;
         }
 
