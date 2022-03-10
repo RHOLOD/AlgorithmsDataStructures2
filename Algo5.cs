cc5a516142be44344132a4703b0be0d523d3a7b9
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2022-03-10 18:15:25 +0500

    Add files via upload

---
+++ Algo5.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+	public static class BalancedBST
+	{
+		public static int[] GenerateBBSTArray(int[] a)
+		{
+			return null;
+		}
+	}
+}
