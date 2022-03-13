b08745742069d0d3b751eacd76db43107a34112f
RHOLOD <42797219+RHOLOD@users.noreply.github.com>
2022-03-13 21:42:54 +0500

    Add files via upload

---
+++ Algo5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+	public static class BalancedBST
+	{
+		public static int[] GenerateBBSTArray(int[] a)
+		{
+			Array.Sort(a);
+			int length = a.Length;
+			int[] array = new int[length];
+			Generate(a, 0);
+
+			void Generate(int[] b, int intexRoot)
+			{
+				if(b.Length != 0)
+                {
+					int index = b.Length / 2;
+					array[intexRoot] = b[index];
+					int[] arrayLeft = new int[index];
+					int[] arrayRight = new int[index];
+					Array.Copy(b, 0, arrayLeft, 0, index);
+					Array.Copy(b, index+1, arrayRight, 0, index);
+					Generate(arrayLeft, intexRoot * 2 + 1);
+					Generate(arrayRight, intexRoot * 2 + 2);
+				}
+			}
+
+			return array;
+		}
+
+	}
+}
